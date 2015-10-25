0ea57ddc691610d6b6c9f1028b6084b1d822e721
vvakame <vvakame+dev@gmail.com>
2015-10-25 15:17:57 +0900

    apmがNode v0.10系でインストールに失敗する現象を修正したつもり

--- setup.sh
+++ setup.sh
@@ -3,6 +3,12 @@
 set -eux
 
 rm -rf node_modules
-apm install
+
+# apm on Node.js v0.10, can't install devDependencies
+# If https://github.com/atom/apm/pull/449 released, when We should use simply `apm install`.
+apm install --production
+npm install
+apm rebuild
+
 grunt setup
 grunt
